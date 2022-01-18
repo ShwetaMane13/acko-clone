c7df630d296afaa36f55fc6f858116d49c156987
Sameel Paikadan <82999575+sameel555@users.noreply.github.com>
2022-01-18 13:32:23 +0530

    landing page middle

---
+++ acko-client/src/components/Homepage/HomeMiddle.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styles from './homeMiddle.module.css'
+import amazon from './images/amazon.png'
+import abhiBus from "./images/abhiBus.png"
+import aubank from "./images/aubank.png"
+import driveU from "./images/driveU.png"
+import dunzo from "./images/dunzo.png"
+import goibibo from "./images/goibibo.png"
+import happay from "./images/happay.png"
+import hdb from "./images/hdb.png"
+import lendingKart from "./images/lendingKart.png"
+import moneytap from "./images/moneytap.png"
+import my from "./images/my.png"
+import niyo from "./images/niyo.png"
+import oyo from "./images/oyo.png"
+import practo from "./images/practo.png"
+import rapido from "./images/rapido.png"
+import redbus from "./images/redbus.png"
+import urban from "./images/urban.png"
+import ola from "./images/ola.png"
+import zestMoney from "./images/zestMoney.png"
+import zomato from "./images/zomato.png"
+import zoomCar from "./images/zoomCar.png"
+
+function HomeMiddle() {
+    return (
+        <div className={styles.home_partner}>
+            <h1 className={styles.home_mid_head}>Have a partner issued policy?</h1>
+            <h4 className={styles.home_mid_subhead}>Login to claim & download your policy</h4>
+            <div className={styles.home_mid_logo}>
+                 
+            </div>
+        </div>
+    )
+}
+
+export default HomeMiddle
