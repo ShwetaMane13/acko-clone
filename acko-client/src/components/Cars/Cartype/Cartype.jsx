56010de9d5da8a9b163bf7bcced0eca1984c6eeb
Yashraj Maheshwari <yashrajmaheshwari15@gmail.com>
2022-01-18 16:02:56 +0530

    cartype

---
+++ acko-client/src/components/Cars/Cartype/Cartype.jsx
@@ -0,0 +1,130 @@
+import { Link } from "react-router-dom";
+import React from "react";
+
+export const Cartype = () => {
+  return (
+    <div>
+      <div className={style.useofcarbody}>
+        <div
+          style={{
+            fontStyle: "normal",
+            fontWeight: "500",
+            fontSize: "24px",
+            lineHeight: "28px",
+            alignItems: "center",
+            textAlign: "center",
+          }}
+        >
+          What do you use car for?
+        </div>
+        <Link
+          style={{ textDecoration: "none", color: "black" }}
+          to="/cars/pincode"
+        >
+          <div
+            onClick={async () => {
+              try {
+                const data = { useofcar: "Personal use" };
+                // const res = await axios.post("http://localhost:8080/cars", data);
+                const res = await axios.post(
+                  "https://acko.herokuapp.com/cars",
+                  data
+                );
+                // console.log(res.data._id);
+                const id = res.data._id;
+                localStorage.setItem("ackoid", id);
+              } catch (err) {
+                console.log(err.message);
+              }
+            }}
+            className={style.personalcardiv}
+          >
+            <div>
+              <img src={percar} style={{ marginTop: "20px" }} alt=""></img>
+            </div>
+            <div>
+              <p
+                style={{
+                  lineHeight: "10px",
+                  fontStyle: "normal",
+                  fontWeight: "bold",
+                  fontSize: "14px",
+                }}
+              >
+                Personal Use
+              </p>
+
+              <p
+                style={{
+                  lineHeight: "10px",
+                  fontStyle: "normal",
+                  fontWeight: "bold",
+                  fontSize: "10px",
+                  color: "#8A909F",
+                }}
+              >
+                White Number Plate
+              </p>
+            </div>
+          </div>
+        </Link>
+        <Link
+          style={{ textDecoration: "none", color: "black" }}
+          to="/cars/pincode"
+        >
+          <div
+            onClick={async () => {
+              try {
+                const data = { useofcar: "Commercial use" };
+                const res = await axios.post(
+                  "https://acko.herokuapp.com/cars",
+                  data
+                );
+                // console.log(res.data._id);
+                const id = res.data._id;
+                localStorage.setItem("ackoid", id);
+              } catch (err) {
+                console.log(err.message);
+              }
+            }}
+            className={style.personalcardiv}
+          >
+            <div>
+              <img src={colorcar} style={{ marginTop: "20px" }} alt=""></img>
+            </div>
+            <div>
+              <p
+                style={{
+                  lineHeight: "10px",
+                  fontStyle: "normal",
+                  fontWeight: "bold",
+                  fontSize: "14px",
+                }}
+              >
+                Commercial Use
+              </p>
+              <p
+                style={{
+                  lineHeight: "10px",
+                  fontStyle: "normal",
+                  fontWeight: "bold",
+                  fontSize: "10px",
+                  color: "#8A909F",
+                }}
+              >
+                Yellow Number Plate
+              </p>
+            </div>
+          </div>
+        </Link>
+
+        <div className={style.handcardiv}>
+          <img src={caronhand} alt="" />
+          <p style={{ fontSize: "14px", fontWeight: "normal" }}>
+            We've insured more than 5 Lakh cars in India till date
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
