0f6d2a6b87cbdc8c8210e7f1f05f1d21310017b2
Vikas Kumar <vsrathod39@gmail.com>
2022-01-19 18:17:20 +0530

    car number authentication added

--- acko-server/router/auth.js
+++ acko-server/router/auth.js
@@ -2,17 +2,40 @@ const express = require("express");
 const router = express.Router();
 const db = require("../database/connection");
 const User = require("../models/userSchema");
+const Cars = require("../models/carSchema");
 
 // Middleware
-const middleware = (req, res, next) => {
-  console.log("Hello middleware");
-  next();
-};
+// const middleware = (req, res, next) => {
+//   console.log("Hello middleware");
+//   next();
+// };
 
-router.get("/", (req, res) => {
-  res.send("Hello world from router server...");
+// car details
+router.get("/cars/details", (req, res) => {
+  const { carNumber, apiKey } = req.query;
+  if (!(apiKey === "test5")) {
+    return res
+      .status(201)
+      .json({
+        message:
+          "please check api key 'http://localhost:5000/cars/details?apiKey=<>&carNumber=<>'",
+      });
+  }
+  Cars.findOne({ carNumber })
+    .then((data) => {
+      if (data === null) {
+        return res.status(201).json({ message: "car number does not exist" });
+      }
+      return res.status(201).json(data);
+    })
+    .catch((error) => {
+      return res
+        .status(500)
+        .json({ message: "please check car number", error: error });
+    });
 });
 
+// user registration
 router.post("/user/register", (req, res) => {
   const { name, email, phone, password } = req.body;
   if (!name || !email || !phone || !password) {
