73810329639d5880ef393d87b7edae15d51e383f
Rajat Kumar <rajatrana.rk@gmail.com>
2022-01-20 12:25:48 +0530

    cars pincode page updage

--- acko-client/src/components/Cars/CarsPincode/CarsPincode.jsx
+++ acko-client/src/components/Cars/CarsPincode/CarsPincode.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+
 import './CarsPincode.css'
 import navigation from './Navigation.svg'
 //import axios from "axios";
@@ -7,37 +8,11 @@ import { Link } from 'react-router-dom'
 function CarsPincode() {
   const [pincode, setPincode] = useState('')
 
-  return (
-    <div className="pincodeDiv">
-      <div className="innerPincodeDiv">Where do you stay?</div>
-      <input
-        onChange={(e) => {
-          setPincode(e.target.value)
-        }}
-        className="pInput"
-        placeholder="160062"
-      />
-      <p className="pDiv">
-        Please enter the pincode where your vehicle is currently located
-      </p>
-      <Link to="/cars/carnumber">
-        <div>
-          <button
-            onClick={async () => {
-              // const id = localStorage.getItem("ackoid");
-              // const data = { pincode: pincode };
-              // await axios.patch(`http://localhost:8080/cars/${id}`, data);
-              //  await axios.patch(
-              //    `https://acko.herokuapp.com/cars/${id}`,
-              //    data
-              //  );
-            }}
-            className="pinbtn"
-          >
-            Continue
-          </button>
+     return (
+      <div className="pincodeDiv">
+        <div className='innerPincodeDiv'>
+          Where do you stay?
         </div>
-
         <input
           onChange={(e) => {
             setPincode(e.target.value)
@@ -45,15 +20,15 @@ function CarsPincode() {
           className="pInput"
           placeholder="160062"
         />
-        <p className="pDiv">
+        <p className='pDiv' >
           Please enter the pincode where your vehicle is currently located
         </p>
-        <Link to="/cars/carnumber">
+        <Link to="/cars/carnumber" >
           <div>
             <button
-              onClick={async () => {
-                const id = localStorage.getItem('ackoid')
-                const data = { pincode: pincode }
+              onClick={() => {
+
+                
               }}
               className="btn1"
             >
@@ -61,15 +36,16 @@ function CarsPincode() {
             </button>
           </div>
         </Link>
-      </Link>
 
-      <div className="pindivflex">
-        <img src={navigation} alt="" />
-        <p className="pInnerDiv">
-          Your pincode helps us check our service availability in your region
-        </p>
+        <div className="pindivflex">
+          <img src={navigation} alt="" />
+          <p className='pInnerDiv'>
+            Your pincode helps us check our service availability in your region
+          </p>
+        </div>
       </div>
-    </div>
+
+     
   )
 }
 
