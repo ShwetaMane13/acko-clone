f2dd34d106f7e539bbc34935bfc8692aad302a29
Vikas Kumar <vsrathod39@gmail.com>
2022-01-21 16:02:15 +0530

    serching car image route added

--- acko-server/router/auth.js
+++ acko-server/router/auth.js
@@ -3,23 +3,15 @@ const router = express.Router();
 const db = require("../database/connection");
 const User = require("../models/userSchema");
 const Cars = require("../models/carSchema");
-
-// Middleware
-// const middleware = (req, res, next) => {
-//   console.log("Hello middleware");
-//   next();
-// };
+const CarPic = require("../models/carPicSchema");
 
 // car details
 router.get("/cars/details", (req, res) => {
   const { carNumber, apiKey } = req.query;
   if (!(apiKey === "test5")) {
-    return res
-      .status(201)
-      .json({
-        message:
-          "please check api key 'http://localhost:5000/cars/details?apiKey=<>&carNumber=<>'",
-      });
+    return res.status(201).json({
+      message: "please check api key",
+    });
   }
   Cars.findOne({ carNumber })
     .then((data) => {
@@ -32,6 +24,55 @@ router.get("/cars/details", (req, res) => {
       return res
         .status(500)
         .json({ message: "please check car number", error: error });
+    });
+});
+
+// cars pic and price value
+router.get("/car/pic", (req, res) => {
+  console.log("hi", req.query);
+  if (Object.keys(req.query).length === 0) {
+    CarPic.find({})
+      .then((data) => {
+        if (data === null) {
+          return res.status(201).json({ message: "no data exist" });
+        }
+        return res.status(201).json(data);
+      })
+      .catch((error) => {
+        return res.status(500).json({ message: "please check route", error });
+      });
+  }
+  const { company, apiKey, model } = req.query;
+  if (!(apiKey === "test5")) {
+    return res.status(201).json({
+      message: "please check api key",
+    });
+  }
+  if (company && model) {
+    CarPic.findOne({ $and: [{ company }, { model }] })
+      .then((data) => {
+        if (data === null) {
+          return res.status(201).json({ message: "car number does not exist" });
+        }
+        return res.status(201).json(data);
+      })
+      .catch((error) => {
+        return res
+          .status(500)
+          .json({ message: "please check company name", error: error });
+      });
+  }
+  CarPic.find({ $or: [{ company }, { model }] })
+    .then((data) => {
+      if (data === null) {
+        return res.status(201).json({ message: "car number does not exist" });
+      }
+      return res.status(201).json(data);
+    })
+    .catch((error) => {
+      return res
+        .status(500)
+        .json({ message: "please check company name", error: error });
     });
 });
 
