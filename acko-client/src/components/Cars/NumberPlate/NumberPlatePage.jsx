b08682f9f0058900e973113dd9c6892da416ad33
Yashraj Maheshwari <yashrajmaheshwari15@gmail.com>
2022-01-23 19:15:50 +0530

    edited number plate page

--- acko-client/src/components/Cars/NumberPlate/NumberPlatePage.jsx
+++ acko-client/src/components/Cars/NumberPlate/NumberPlatePage.jsx
@@ -2,19 +2,21 @@ import NumberPlate from "./NumberPlate";
 import Header from "../../Headers/Hearder";
 import Middle from "../../Middle/Middle";
 import "../carstypepage.css";
+import { Dynamic } from "../Dynamic/Dynamic";
 
 export const NumberPlatePage = () => {
-    return (
-      <div>
-        <Header />
-        <Middle />
-        <div className="divflex">
-          <div className="divflex1">
-           <NumberPlate/>
-          </div>
-          <div className="divflex2"></div>
+  return (
+    <div>
+      <Header />
+      <Middle />
+      <div className="divflex">
+        <div className="divflex1">
+          <NumberPlate />
+        </div>
+        <div className="divflex2">
+          <Dynamic />
         </div>
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
