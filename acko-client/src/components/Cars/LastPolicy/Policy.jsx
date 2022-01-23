240dca390410cc06b6dedb9487014d6b39dfd0ba
Yashraj Maheshwari <yashrajmaheshwari15@gmail.com>
2022-01-23 16:21:53 +0530

    css last policy page

--- acko-client/src/components/Cars/LastPolicy/Policy.jsx
+++ acko-client/src/components/Cars/LastPolicy/Policy.jsx
@@ -16,29 +16,27 @@ export const Policy = ({ setpopupp }) => {
         Did you make a claim in last policy period?
       </div>
       <div style={{ marginTop: "40px" }} className={style.policygrid}>
-        <div onClick={() => setitem("0")}>
-          <button>Yes</button>
-        </div>
-        <div onClick={() => setitem("20")}>
-          <button
-            onClick={() => {
-              setpopupp(true);
-            }}
-            style={{ marginLeft: "20px" }}
-          >
-            No
-          </button>
-        </div>
-        <div onClick={() => setitem("50")}>
-          <button
-            onClick={() => {
-              setpopupp(true);
-            }}
-            style={{ marginLeft: "20px" }}
-          >
-            I have never made a claim
-          </button>
-        </div>
+        <button onClick={() => setitem("0")}>Yes</button>
+
+        <button
+          onClick={() => {
+            setitem("20");
+            setpopupp(true);
+          }}
+          style={{ marginLeft: "20px" }}
+        >
+          No
+        </button>
+
+        <button
+          onClick={() => {
+            setitem("50");
+            setpopupp(true);
+          }}
+          style={{ marginLeft: "20px" }}
+        >
+          I have never made a claim
+        </button>
       </div>
       <div className={style.policyflexdiv}>
         <img src={thumbsup} alt="" />
